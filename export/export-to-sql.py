5ef1047642201bd96b9e6fa481b39e3661f0ac7c
Victor Cruz <vicdan215@gmail.com>
2020-05-08 14:56:37 -0500

    Default noeco

--- export/export-to-sql.py
+++ export/export-to-sql.py
@@ -19,6 +19,8 @@ results = collection.find(query)
 
 for x in results:
     sql = "INSERT INTO routers (noeco, mac, email, edad, cp, genero) VALUES (%s, %s, %s, %s, %s, %s)"
+    if x.noeco is None:
+      x.noeco = ""
     val = (x.noeco, x.mac, x.email, x.edad, x.cp, x.genero)
     sqlcursor.execute(sql, val)
     sqlConnection.commit()
